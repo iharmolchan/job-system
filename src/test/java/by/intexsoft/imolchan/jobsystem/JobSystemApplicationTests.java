a27b48d1ea3a01ae9493c653bd3344e3706b73f1
Ihar Molchan <ihar.molchan@intexsoft.by>
2022-05-13 15:48:52 +0300

    Add initial project code with dependencies

---
+++ src/test/java/by/intexsoft/imolchan/jobsystem/JobSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.intexsoft.imolchan.jobsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
