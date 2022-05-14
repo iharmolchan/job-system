6585202559657f69689e1818bc80b6af079859ad
Ihar Molchan <ihar.molchan@intexsoft.by>
2022-05-14 17:38:35 +0300

    Add dto converter util

--- src/main/java/by/intexsoft/imolchan/jobsystem/util/DtoConverter.java
+++ src/main/java/by/intexsoft/imolchan/jobsystem/util/DtoConverter.java
@@ -3,12 +3,17 @@ package by.intexsoft.imolchan.jobsystem.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DtoConverter {
     private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
-    public static  <T> T convert(Object obj, Class<T> clazz) {
+    static {
+        MODEL_MAPPER.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+    }
+
+    public static <T> T convert(Object obj, Class<T> clazz) {
         return MODEL_MAPPER.map(obj, clazz);
     }
 }
