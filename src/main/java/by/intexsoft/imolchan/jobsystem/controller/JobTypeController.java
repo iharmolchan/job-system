72b690952a1761a90152dc3912f065b4c31edc16
Ihar Molchan <ihar.molchan@intexsoft.by>
2022-05-16 22:38:13 +0300

    Change response status for job type creation method

--- src/main/java/by/intexsoft/imolchan/jobsystem/controller/JobTypeController.java
+++ src/main/java/by/intexsoft/imolchan/jobsystem/controller/JobTypeController.java
@@ -6,8 +6,17 @@ import by.intexsoft.imolchan.jobsystem.views.CrudView;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -20,6 +29,7 @@ public class JobTypeController {
     private final JobTypeService jobTypeService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public JobTypeDTO createJobType(@JsonView(CrudView.CREATE.class) @Valid @RequestBody JobTypeDTO jobTypeDTO) {
         log.info("Creating job type: {}", jobTypeDTO);
         return jobTypeService.saveJobType(jobTypeDTO);
